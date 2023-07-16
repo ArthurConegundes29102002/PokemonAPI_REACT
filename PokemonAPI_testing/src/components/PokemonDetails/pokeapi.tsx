6aa277b9e9324fc5d567556b87b86aa6437fbf55
Tanha <acmsp@cin.ufpe.br>
2023-07-16 19:45:02 -0300

    código agora exibe mensagem de erro caso a API falhe na requisição

--- PokemonAPI_testing/src/components/PokemonDetails/pokeapi.tsx
+++ PokemonAPI_testing/src/components/PokemonDetails/pokeapi.tsx
@@ -7,46 +7,45 @@ export default function PokemonDetails() {
   const [data, setData] = useState<any>(null);
   const [data_img, setDataIMG] = useState<any>(null);
   const [data_type, setDataTYPE] = useState<any>(null);
-  // const [pokemon_attack, setDataHabilities] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
   
-  // ------------------------------------------------- implementando o UseEffect:
-
   useEffect(() => {
     if (userInput) {
       Catching_Data();
     } else {
       setData(null);
       setDataIMG(null);
-      // setDataTYPE([null]);
-      setDataTYPE(null)
+      setDataTYPE(null);
+      setError(null);
     }
   }, [userInput]);
-
-// -------------------------------------------------------------------------------------------------- Definindo constante para receber os dados da API:
 
   const Catching_Data = async () => {
     try {
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${userInput}`);
 
-      // --------------------------------------------Separando os tipos de dados:
+      let type_array: string[] = [];
+      const { data } = response;
+      data.types.map((e: any) => { 
+        const type: string = e.type.name;
+        type_array.push(type);
+      });
+      let type_array_result:string = type_array.join(' - ');
 
-      let type_array:String[]=[]
-      console.log(response)
-      const {data} = response
-      data.types.map((e:any)=>{ 
-        const type:string= e.type.name //nome do tipo do pokemon
-        type_array.push(type)
-      })
-      let type_array_result = type_array.join(' ') //aqui eu estou separando os valores que saem do "type array" com um espaço evitando que fiquem juntos
-      
       setData(response.data);
-      setDataIMG(response.data.sprites.front_default)
-      setDataTYPE(type_array_result)
+      setDataIMG(response.data.sprites.front_default);
+      setDataTYPE(type_array_result);
 
+      setError(null); // Limpa o erro se a solicitação for bem-sucedida
 
-      // --------------------------------------------
     } catch (error) {
       console.error('Error catching the response:', error);
+      setError('Infelizmente esse pokemon ainda não existe :('); // Define a mensagem de erro"
+
+      // limpando os elementos após o erro de requisição
+      setData(null);
+      setDataIMG(null);
+      setDataTYPE(null);
     }
   };
 
@@ -54,18 +53,19 @@ export default function PokemonDetails() {
     setUserInput(event.target.value);
   };
 
-// --------------------------------------------------------------------------------------------------
   return (
     <div className='big_container'>
+      <input className='inputbox' type="text" value={userInput} onChange={handleInputChange} placeholder="digite um número..." />
 
-      <input  className='inputbox' type="text" value={userInput} onChange={handleInputChange} placeholder="digite um número..." />
+      {error && <h2>Error: {error}</h2>}
 
-      {data && data.name && <h1>Name: {data.name}</h1>}
-
-      <img src={data_img}/>
-      
-      <h2>Tipo do Pokemon: {data_type}</h2>
-
+      {data && data.name && (
+        <div>
+          <h1>Name: {data.name}</h1>
+          <img src={data_img} alt={data.name} />
+          <h2>Tipo do Pokemon: {data_type}</h2>
+        </div>
+      )}
     </div>
   );
 }
